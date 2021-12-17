154b73ba4805d5eb626e2423fc0e7d3c472c8545
Teslime <teslime_mlpnr@outlook.com>
2021-12-17 13:21:28 +0300

    Added ListingPhonebook design.

--- ListingPhonebook.Designer.cs
+++ ListingPhonebook.Designer.cs
@@ -29,19 +29,76 @@ namespace Phonebook
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.PersonId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.NameSurname = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Number = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.PersonId,
+            this.NameSurname,
+            this.Number});
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listView1.HideSelection = false;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1});
+            this.listView1.Location = new System.Drawing.Point(37, 79);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(441, 325);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // PersonId
+            // 
+            this.PersonId.Text = "Person ID";
+            this.PersonId.Width = 89;
+            // 
+            // NameSurname
+            // 
+            this.NameSurname.Text = "Name Surname";
+            this.NameSurname.Width = 164;
+            // 
+            // Number
+            // 
+            this.Number.Text = "Number";
+            this.Number.Width = 181;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(34, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Phonebook :";
             // 
             // ListingPhonebook
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
             this.Name = "ListingPhonebook";
             this.Text = "Listing Phonebook";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader PersonId;
+        private System.Windows.Forms.ColumnHeader Number;
+        public System.Windows.Forms.ColumnHeader NameSurname;
+        private System.Windows.Forms.Label label1;
     }
 }
